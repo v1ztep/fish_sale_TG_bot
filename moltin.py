9f7bc5e8f3c95188655a56d9814b18c371bd5e5e
v1ztep <v1ztep@gmail.com>
2021-07-31 00:07:26 +0300

    Добавил get_image

--- moltin.py
+++ moltin.py
@@ -3,9 +3,9 @@ import os
 import time
 
 import requests
-# from dotenv import load_dotenv
-#
-# load_dotenv()
+from dotenv import load_dotenv
+
+load_dotenv()
 
 EP_ACCESS_TOKEN = None
 EP_TOKEN_LIFETIME = None
@@ -58,6 +58,18 @@ def get_product(moltin_token, product_id):
     return response.json()
 
 
+def get_image(moltin_token, image_id):
+    access_token = get_ep_access_token(moltin_token)
+    headers = {
+        'Authorization': f'Bearer {access_token}',
+    }
+    response = requests.get(f'https://api.moltin.com/v2/files/{image_id}',
+                            headers=headers)
+    response.raise_for_status()
+    image = response.json()['data']['link']['href']
+    return image
+
+
 def add_product_to_cart(moltin_token, cart_id):
     cart_tg_id = f'tg{cart_id}' #<<<<<<<<убрать строку, передать готовый уникальный ID<<<<<<<<<<<<
     url = f'https://api.moltin.com/v2/carts/{cart_tg_id}/items'
@@ -77,13 +89,13 @@ def add_product_to_cart(moltin_token, cart_id):
 
 
 def get_cart(moltin_token, cart_id):
-    cart_tg_id = f'tg{cart_id}'  # <<<<<<<<убрать строку, передать готовый уникальный ID<<<<<<<<<<<<
-    url = f'https://api.moltin.com/v2/carts/{cart_tg_id}'
+    cart_tg_id = f'tg{cart_id}'  # <<<<<<<<убрать строку, передать готовый уникальный ID<<<<<<
     access_token = get_ep_access_token(moltin_token)
     headers = {
         'Authorization': f'Bearer {access_token}',
     }
-    response = requests.get(url, headers=headers)
+    response = requests.get(f'https://api.moltin.com/v2/carts/{cart_tg_id}',
+                            headers=headers)
     response.raise_for_status()
     return response.json()
 
@@ -108,7 +120,8 @@ def main():
     add_product = add_product_to_cart(moltin_token, tg_chat_id)
     cart = get_cart(moltin_token, tg_chat_id)
     items = get_cart_items(moltin_token, tg_chat_id)
-    get_product_info = get_product(moltin_token, product_id)
+    get_product_info = get_product(moltin_token, )
+    image = get_image(moltin_token, )
 
     with open('response.json', "w", encoding='utf8') as file:
         json.dump(get_product_info, file, ensure_ascii=False, indent=4)
